7d6032c109a40fe6d43c14f4601aa87bf9ddc796
winnieotieno770 <winnieotieno770@gmail.com>
2025-12-30 17:10:09 +0300

    fix take_a_number position logic

--- lib/deli_counter.py
+++ lib/deli_counter.py
@@ -1,9 +1,17 @@
+import ipdb
 def line(customer_list):
     if len(customer_list) == 0:
         print("The line is currently empty.")
-   
+        return
+    customers = " ".join(f"{index + 1}. {name}" for index, name in enumerate(customer_list))
+    print(f'The line is currently: {customers}')
     
-def take_a_number():
-    pass
+def take_a_number(customer_list, customer_name):
+    customer_list.append(customer_name)
+    num = len(customer_list)
+    print(f"Welcome, {customer_name}. You are number {num} in line.")
+
+
 def now_serving():
     pass
+take_a_number(["Brenda", "Anna"], "Clark")
